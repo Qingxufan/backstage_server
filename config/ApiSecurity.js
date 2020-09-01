cb485324a15cdd281bcfc839c1769b9ec5766391
fanqingxu <qingxufan0923@163.com>
2020-09-01 17:27:33 +0800

    2020.9.1/2

--- config/ApiSecurity.js
+++ config/ApiSecurity.js
@@ -1,9 +1,10 @@
 module.exports = {
   // 转发的路径
+
+  //game
   '/backstage/game/node_list': 2,
   '/backstage/game/get_node_info': 2,
   '/backstage/game/create_node': 2,
-  '/backstage/game/get_node_info': 2,
   '/backstage/game/update_node': 2,
   '/backstage/game/remove_node': 2,
   '/backstage/game/game_list': 2,
@@ -11,7 +12,6 @@ module.exports = {
   '/backstage/game/create_game': 2,
   '/backstage/game/update_game': 2,
   '/backstage/game/get_room_info': 2,
-  '/backstage/game/create_room': 2,
   '/backstage/game/create_room': 2,
   '/backstage/game/update_game_admin_status': 1,
   '/backstage/game/update_game_status': 2,
@@ -21,6 +21,36 @@ module.exports = {
   '/backstage/game/update_room_status': 2,
   '/backstage/game/publish_game': 2,
   '/backstage/game/publish_room': 2,
+  '/backstage/msg/send_world_chat': 2,
+  '/backstage/manage/get_uc': 2,
+  '/backstage/manage/set_uc': 2,
+
+   //app
+   "/backstage/sys/app_config":2,
+   "/backstage/manage/sys_config_list":2,
+   "/backstage/manage/update_sys_config":2,
+   "/backstage/sys/create_sys_config":2,
+   "/backstage/manage/get_fb_secret_list":2,
+   "/backstage/manage/set_fb_secret":2,
+   "/backstage/manage/remove_fb_secret":2,
+
+   //activity
+   "/backstage/activity/get_turntable_config":2,
+   "/backstage/activity/set_turntable_config":2,
+
+   //mail
+   "/backstage/mail/send_mail":2,
+
+   //statistic
+   "/statistic/list_goldchange":2,
+   "/statistic/list_goldChangeNoGame":2,
+   "/statistic/list_giveAndReceive":2,
+   "/statistic/list_playerAccount":2,
+   "/backstage/user/get_user_info_bwb":2,
+   "/statistic/list_realTimeData":2,
+   "/statistic/list_gameRecord":2,
+   "/statistic/list_dataSummary":2,
+
 
   //本地服务
   '/user/list':2,
